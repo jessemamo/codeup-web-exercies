438117264cba1e839934e77965bdd1cc5238ec1c
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-04-07 19:35:28 -0400

    finished exercise

---
+++ js/external.js
@@ -0,0 +1,25 @@
+//console.log ('Hello from external JavaScript')
+//alert('Welcome to my Website!')
+//var thatsMyFavColorToo = prompt("whats your fav color?");
+//
+//alert(thatsMyFavColorToo + " is my fav color too")
+//var daysWithLittleMermaid= prompt("how many days did you have little mermaid?")
+//var daysWithBrotherBear= prompt("how many days did you have brother bear")
+//var daysWithHercules= prompt("how many days did you have hercules")
+//var totalCost= ((daysWithLittleMermaid +git add daysWithBrotherBear + daysWithHercules ) * 3)
+//alert("days with movies will cost" + totalCost + dollars)
+//var payperhourGoogle =400  parsefloat(number("what is the total hours worked for google"))
+//var payperhourfacebook= 350 parsefloat(number("what is the total hours worked for facebook"))
+//var payperhouramazon= 380 parsefloat(number("what is the total hours worked for amazon"))
+//var totalDollarsEarnedHourly = (payperhourGoogle * 6)(payperhouramazon * 4)(payperhourfacebook * 10) alert('how pay per hour total')
+//
+//var classNotFull= confirm("is the class full")
+//var classSchCheck=confirm('do you have time in schedule')
+//var studentEnrolled= confirm("is the student enrolled")
+//alert('if all checks out.. you can enroll')
+var hasMoreThanTwoItemsOrMore= confirm('how many items do you have?')
+var isOfferExpired= confirm('is the offer expired?')
+var isPremiumMember= confirm ("are you a premium member?")
+alert("product discount applies if you buy 3 or more items and are a premium member..")
+
+
